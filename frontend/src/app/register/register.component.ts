22ff1866efd2fc5f21ce0264b820ca298a0ad59d
Moritz Weirauch <m.weirauch@gmx.ch>
2019-04-30 16:58:02 +0200

    .

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -8,8 +8,10 @@ import {FormControl, Validators} from "@angular/forms";
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm: FormGroup;
 
   constructor(
+    private _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
