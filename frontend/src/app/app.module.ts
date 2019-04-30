5e54cea22059cc9985ad60a562704fd28d2f36e2
Mikel Prandini <mikel.prandini@swisscom.com>
2019-05-01 00:18:12 +0800

    Login page finished.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,7 +26,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule,
+    // MaterialModule,
     FormsModule,
     ReactiveFormsModule,
 
