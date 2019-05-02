deb2259cc1d1edadf73d4cb76e2c654ad3449870
Moritz Weirauch <m.weirauch@gmx.ch>
2019-05-02 13:17:14 +0200

    Adapted Routes File

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const userController = require('../controllers/UserController');
+const deviceController = require('../controllers/DeviceController');
 
 const userUrl = '/user';
 const deviceUrl = '/device';
