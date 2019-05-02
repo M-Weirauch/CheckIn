d3e5d621fdb9f66af2647b31e2602aa93ad02223
Moritz Weirauch <m.weirauch@gmx.ch>
2019-05-02 13:07:07 +0200

    Added Device Route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -4,6 +4,7 @@ const app = express();
 const userController = require('../controllers/UserController');
 
 const userUrl = '/user';
+const deviceUrl = '/device';
 
 // Get User by given userId
 app.get(userUrl, userController.getUserById);
