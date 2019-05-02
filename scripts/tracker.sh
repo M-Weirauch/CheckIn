a6e2fe921566099a03b6749a441a7b8ecfbc0fdc
Moritz Weirauch <m.weirauch@gmx.ch>
2019-05-02 10:16:07 +0200

    Improvement of Tracking Script

--- scripts/tracker.sh
+++ scripts/tracker.sh
@@ -1,55 +1,65 @@
 # ********************************************
-#           Name: MAC Adress Tracking       *
-#           Author: Moritz Weirauch         *
-#           Version: 0.1                    *
-# *******************************************
+#           Name: MAC Adress Tracking        *
+#           Author: Moritz Weirauch          *
+#           Version: 0.1                     *
+# ********************************************
+
+# ******************
+
+	# Store Current List into File 
+	
+		currList = `wl -i <interface> asoclist`
+		currList < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/curassoc.txt
 
 # ******************
 # Variables
 
-
-    file=”/var/log/assoc”
-    oldList=`cat file`
-    newList=`wl -i <interface> asoclist`
-    oldListArray=($oldList)
-    newListArray($newList)
+	file=/c/Users/Moritz/Documents/GitHub/CheckIn/scripts/assoc.txt
+	mapfile -t oldListArray < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/assoc.txt
+	mapfile -t newListArray < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/curassoc.txt
 
 # ******************
 # Track lost devices
 
-    oldDevices=()
-    for i in oldListArray; do 
-        skip=
-        for j in newListArray; do
-            [[ $i == $j ]] && { skip=1;break; }
-        done
-        [[ -n $skip ]] || oldDevices+=(“$i”)
-    done
-    declare -p oldDevices
-    # Push the Device List
-    for i in oldDevices do
-        curl {mac: $i}  https://dummyurl.com/api/device/
-    done
+	# Determine Devices that left
+
+	oldDevices=()
+	for i in "${oldListArray[@]}"; do
+		skip=
+		for j in "${newListArray[@]}"; do
+			[[ $i == $j ]] && { skip=1; break; }
+		done
+		[[ -n $skip ]] || oldDevices+=("$i")
+	done
+	declare -p oldDevices
+	
+	# Push all the Old ones to the API
+	
+	for i in "${oldDevices[@]}"; do
+		echo $i
+	done
 
 # ********************
 # Track new devices
 
-    # Find Devices that are new
-    newDevices=()
-    for i in newListArray; do 
-        skip=
-        for j in oldListArray; do
-            [[ $i == $j ]] && { skip=1;break; }
-        done
-        [[ -n $skip ]] || newDevices+=(“$i”)
-    done
-    declare -p newDevices
-    # Push the Device List
-    for i in newDevices do
-        curl {mac: $i}  https://dummyurl.com/api/device/
-    done
+	newDevices=()
+	for i in "${newListArray[@]}"; do
+		skip=
+		for j in "${oldListArray[@]}"; do
+			[[ $i == $j ]] && { skip=1; break; }
+		done
+		[[ -n $skip ]] || newDevices+=("$i")
+	done
+	declare -p newDevices
+	
+	# Push all the new Ones to the APi
+	
+	#for i in "${newDevices[@]}"; do
+	#	echo $i
+	#done
+
 
 # *********************
 # Refresh "Old" assoc List
 
-    newList > file 
+    currList > file 
