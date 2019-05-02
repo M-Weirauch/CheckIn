8e5b5080097fe4bb2023942b096e97c150108acd
Moritz Weirauch <m.weirauch@gmx.ch>
2019-05-02 10:26:11 +0200

    Improved tracking script

--- scripts/tracker.sh
+++ scripts/tracker.sh
@@ -5,18 +5,36 @@
 # ********************************************
 
 # ******************
+# Preparations
+
+	# Variables 
+	
+		rdir="/var/checkin"
+
+	# Create Files if the do not yet exist
+		
+		# Holder Directory
+		mkdir /var/checkin
+		
+		# Create file to store "old" list 
+		touch $rdir/assoc.txt
+		adir="${rdir}/assoc.txt"
+		
+		# Create file to store "new" list
+		touch $rdir/curassoc.txt
+		cdir="${rdir}/curassoc.txt"
 
 	# Store Current List into File 
 	
 		currList = `wl -i eth1 assoclist`
-		currList < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/curassoc.txt
+		currList < $cdir
 
 # ******************
 # Variables
 
 	file=/c/Users/Moritz/Documents/GitHub/CheckIn/scripts/assoc.txt
-	mapfile -t oldListArray < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/assoc.txt
-	mapfile -t newListArray < /c/Users/Moritz/Documents/GitHub/CheckIn/scripts/curassoc.txt
+	mapfile -t oldListArray < $adir
+	mapfile -t newListArray < $cdir
 
 # ******************
 # Track lost devices
@@ -62,4 +80,4 @@
 # *********************
 # Refresh "Old" assoc List
 
-    currList > file 
+    currList > $adir
